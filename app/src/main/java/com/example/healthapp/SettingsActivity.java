c880275acab2c6dddf324f37e65f73f14f493a4f
Ali <aliranjha987@gmail.com>
2024-04-22 17:21:54 -0400

    ar commit 2

--- app/src/main/java/com/example/healthapp/SettingsActivity.java
+++ app/src/main/java/com/example/healthapp/SettingsActivity.java
@@ -16,22 +16,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings); // Assuming your layout file is named activity_settings.xml
 
-        // Assuming forwardArrowImg is your ImageButton
-        ImageButton forwardArrowImg = findViewById(R.id.forwardArrowImg);
-
-        // Load the original image resource
-        @SuppressLint("UseCompatLoadingForDrawables")
-        Drawable originalDrawable = getResources().getDrawable(R.drawable.forward_arrow);
-
-        // Calculate the desired width and height for the image
-        int desiredWidth = 50; // Set your desired width
-        int desiredHeight = 50; // Set your desired height
-
-        // Scale down the original image to the desired dimensions while maintaining aspect ratio
-        Drawable scaledDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(((BitmapDrawable) originalDrawable).getBitmap(), desiredWidth, desiredHeight, true));
-
-        // Set the scaled drawable as the image source for the ImageButton
-        forwardArrowImg.setImageDrawable(scaledDrawable);
     }
 }
 
