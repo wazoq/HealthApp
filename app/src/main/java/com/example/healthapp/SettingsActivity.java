7a95dfdac5f0f83e999f6e46191936f8bc81e2c3
Ali <aliranjha987@gmail.com>
2024-04-22 17:56:18 -0400

    ar commit 3

--- app/src/main/java/com/example/healthapp/SettingsActivity.java
+++ app/src/main/java/com/example/healthapp/SettingsActivity.java
@@ -5,16 +5,45 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Boolean darkMode;
+    Boolean notifications;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings); // Assuming your layout file is named activity_settings.xml
+        setContentView(R.layout.activity_settings);
+
+        Switch darkModeSwitch = findViewById(R.id.darkModeSwitch);
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    darkMode = true;
+                } else {
+                    darkMode = false;
+                }
+            }
+        });
+
+        Switch notificationsSwitch = findViewById(R.id.notifications_switch);
+        notificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    notifications = true;
+                } else {
+                    notifications = false;
+                }
+            }
+        });
 
     }
 }
