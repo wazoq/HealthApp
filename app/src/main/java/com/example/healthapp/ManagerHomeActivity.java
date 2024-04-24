c98488976bfb903c7f7cc9a3b5859c749ca371a4
Ali <aliranjha987@gmail.com>
2024-04-24 18:08:48 -0400

    send notif

--- app/src/main/java/com/example/healthapp/ManagerHomeActivity.java
+++ app/src/main/java/com/example/healthapp/ManagerHomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.healthapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,21 @@ public class ManagerHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manager_home);
+
+        LinearLayout sendNotificationBox = findViewById(R.id.send_notification_box);
+        sendNotificationBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to the login page
+                Intent intent = new Intent(ManagerHomeActivity.this, SendNotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
+
 }
