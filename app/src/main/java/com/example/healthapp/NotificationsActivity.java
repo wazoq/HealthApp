2f3de50677a59186a5e3755b12e73e2b4cb52755
Ali <aliranjha987@gmail.com>
2024-05-06 18:11:14 -0400

    swipe delete noti2

--- app/src/main/java/com/example/healthapp/NotificationsActivity.java
+++ app/src/main/java/com/example/healthapp/NotificationsActivity.java
@@ -42,7 +42,7 @@ public class NotificationsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notifications);
         onClickUpdate();
     }
