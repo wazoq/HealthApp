a414c5d12dd045c1b364ce08a9b3833608b533b4
Ali <aliranjha987@gmail.com>
2024-05-06 20:24:09 -0400

    hoome screen done

--- app/src/main/java/com/example/healthapp/HomeActivity.java
+++ app/src/main/java/com/example/healthapp/HomeActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,19 +19,255 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HomeActivity extends AppCompatActivity {
 
 //    TextView quoteBox = findViewById(R.id.quote);
+//test
+// Define a counter for completed tasks
+    AtomicInteger completedTasks = new AtomicInteger(0);
+
+    // Define the total TextView
+    TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        totalTextView = findViewById(R.id.totalText);
         updateName();
         updateDate();
+        updateData();
+    }
+
+    public void updateData() {
+        // Get today's date in the format "MM-DD-YYYY"
+        String todayDate = getCurrentDate();
+        AtomicInteger total = new AtomicInteger();
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userEmail = user.getEmail();
+        CollectionReference collectionReference = firestore.collection(userEmail);
+
+        // Get document "Running" from the collection
+        collectionReference.document("Running").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    // DocumentSnapshot contains the data of the document
+                    Map<String, Object> data = document.getData();
+                    if (data != null) {
+                        // Check if today's date exists as a field in the document
+                        if (data.containsKey(todayDate)) {
+                            // Retrieve the value associated with today's date
+                            Object value = data.get(todayDate);
+                            // Handle the value as needed
+                            // For example:
+                            if (value != null) {
+                                // Update the TextView with the value
+                                runOnUiThread(() -> {
+                                    TextView runningText = findViewById(R.id.RunningText);
+                                    runningText.setText(value.toString() + " Minutes");
+                                    int intValue = Integer.parseInt(value.toString());
+                                    total.addAndGet(intValue);
+                                });
+                            }
+                        } else {
+                            // Today's date is not present in the document
+                            // Handle this case accordingly
+                            Log.d(TAG, "Today's date is not present in the document");
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Document 'Running' does not exist");
+                }
+            } else {
+                Log.d(TAG, "Failed to retrieve document 'Running'", task.getException());
+            }
+            checkTasksCompleted(total.get());
+        });
+
+        collectionReference.document("Walking").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    // DocumentSnapshot contains the data of the document
+                    Map<String, Object> data = document.getData();
+                    if (data != null) {
+                        // Check if today's date exists as a field in the document
+                        if (data.containsKey(todayDate)) {
+                            // Retrieve the value associated with today's date
+                            Object value = data.get(todayDate);
+                            // Handle the value as needed
+                            // For example:
+                            if (value != null) {
+
+                                runOnUiThread(() -> {
+                                    TextView walking_text = findViewById(R.id.WalkingText);
+                                    walking_text.setText(value.toString() + " Minutes");
+                                    int intValue = Integer.parseInt(value.toString());
+                                    total.addAndGet(intValue);
+                                });
+
+                            }
+                        } else {
+                            // Today's date is not present in the document
+                            // Handle this case accordingly
+                            Log.d(TAG, "Today's date is not present in the document");
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Document 'Walking' does not exist");
+                }
+            } else {
+                Log.d(TAG, "Failed to retrieve document 'Walking'", task.getException());
+            }
+            checkTasksCompleted(total.get());
+        });
+
+        collectionReference.document("Weight Lifting").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    // DocumentSnapshot contains the data of the document
+                    Map<String, Object> data = document.getData();
+                    if (data != null) {
+                        // Check if today's date exists as a field in the document
+                        if (data.containsKey(todayDate)) {
+                            // Retrieve the value associated with today's date
+                            Object value = data.get(todayDate);
+                            // Handle the value as needed
+                            // For example:
+                            if (value != null) {
+
+                                runOnUiThread(() -> {
+                                    TextView weights_text = findViewById(R.id.weightliftingText);
+                                    weights_text.setText(value.toString() + " Minutes");
+                                    int intValue = Integer.parseInt(value.toString());
+                                    total.addAndGet(intValue);
+                                });
+
+
+                                // Update the TextView with the value
+
+                            }
+                        } else {
+                            // Today's date is not present in the document
+                            // Handle this case accordingly
+                            Log.d(TAG, "Today's date is not present in the document");
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Document 'Weight Lifting' does not exist");
+                }
+            } else {
+                Log.d(TAG, "Failed to retrieve document 'Weight Lifting'", task.getException());
+            }
+            checkTasksCompleted(total.get());
+        });
+
+        collectionReference.document("Yoga").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    // DocumentSnapshot contains the data of the document
+                    Map<String, Object> data = document.getData();
+                    if (data != null) {
+                        // Check if today's date exists as a field in the document
+                        if (data.containsKey(todayDate)) {
+                            // Retrieve the value associated with today's date
+                            Object value = data.get(todayDate);
+                            // Handle the value as needed
+                            // For example:
+                            if (value != null) {
+
+                                runOnUiThread(() -> {
+                                    TextView yoga_text = findViewById(R.id.yogaText);
+                                    yoga_text.setText(value.toString() + " Minutes");
+                                    int intValue = Integer.parseInt(value.toString());
+                                    total.addAndGet(intValue);
+                                });
+
+
+                                // Update the TextView with the value
+
+                            }
+                        } else {
+                            // Today's date is not present in the document
+                            // Handle this case accordingly
+                            Log.d(TAG, "Today's date is not present in the document");
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Document 'Yoga' does not exist");
+                }
+            } else {
+                Log.d(TAG, "Failed to retrieve document 'Yoga'", task.getException());
+            }
+            checkTasksCompleted(total.get());
+        });
+
+        collectionReference.document("Stairs").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    // DocumentSnapshot contains the data of the document
+                    Map<String, Object> data = document.getData();
+                    if (data != null) {
+                        // Check if today's date exists as a field in the document
+                        if (data.containsKey(todayDate)) {
+                            // Retrieve the value associated with today's date
+                            Object value = data.get(todayDate);
+                            // Handle the value as needed
+                            // For example:
+                            if (value != null) {
+
+                                runOnUiThread(() -> {
+                                    TextView stairs_text = findViewById(R.id.stairsText);
+                                    stairs_text.setText(value.toString() + " Minutes");
+                                    int intValue = Integer.parseInt(value.toString());
+                                    total.addAndGet(intValue);
+                                });
+                            }
+                        } else {
+                            // Today's date is not present in the document
+                            // Handle this case accordingly
+                            Log.d(TAG, "Today's date is not present in the document");
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "Document 'Stairs' does not exist");
+                }
+            } else {
+                Log.d(TAG, "Failed to retrieve document 'Stairs'", task.getException());
+            }
+            checkTasksCompleted(total.get());
+        });
+    }
+
+    private String getCurrentDate() {
+        // Get today's date
+        Date today = Calendar.getInstance().getTime();
+        // Format the date as "MM-DD-YYYY"
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy", Locale.getDefault());
+        return dateFormat.format(today);
+    }
+
+    // test
+    private void updateTotalTextView(int total) {
+        runOnUiThread(() -> totalTextView.setText(total + " Minutes"));
+    }
+
+    private void checkTasksCompleted(int total) {
+        int completed = completedTasks.incrementAndGet();
+        if (completed == 5) { // Assuming there are 6 asynchronous tasks
+            updateTotalTextView(total);
+        }
     }
 
     public void updateDate() {
@@ -84,7 +322,7 @@ public class HomeActivity extends AppCompatActivity {
         // Here, you can use the retrieved name value as needed
         // For example, you can update a TextView with the name
         TextView welcomeTextView = findViewById(R.id.WelcomeText);
-        welcomeTextView.setText("Welcome " + name);
+        welcomeTextView.setText("Welcome " + name + "!");
     }
 
     public void onClickSettings(View view) {
