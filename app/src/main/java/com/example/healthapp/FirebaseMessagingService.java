df7eac7577463684c3f27374c2c10044cb6370b3
Hamza <hamzachaudhrycs@gmail.com>
2024-05-13 19:37:24 -0400

    fixed firebase error

--- app/src/main/java/com/example/healthapp/FirebaseMessagingService.java
+++ app/src/main/java/com/example/healthapp/FirebaseMessagingService.java
@@ -135,7 +135,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.logo)
+                        .setSmallIcon(R.drawable.logosvg)
                         .setContentTitle("New Notification")
                         .setContentText(messageBody)
                         .setAutoCancel(true)
